6fdf8e76d0498e8dc1f7ca044c96eaf588ff1ed2
jquac015 <jquac015@ucr.edu>
2020-10-06 14:25:39 -0700

    Add main2, c-echo, updated .gitignore

---
+++ .gitignore/CMakeFiles/bigger_area_calculator.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/csmajs/jquac015/CS100-lab1/src/new_main.cpp" "/home/csmajs/jquac015/CS100-lab1/CMakeFiles/bigger_area_calculator.dir/src/new_main.cpp.o"
+  "/home/csmajs/jquac015/CS100-lab1/src/rectangle.cpp" "/home/csmajs/jquac015/CS100-lab1/CMakeFiles/bigger_area_calculator.dir/src/rectangle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
